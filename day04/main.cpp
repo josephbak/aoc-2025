5ebadfcb1f44af56ca5398bacd5ee879a7c838b6
Joseph Bak <joseph.bak31415@gmail.com>
2025-12-09 15:42:33 -0500

    2025 advent of code day04 part1

---
+++ day04/main.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <string>
+#include <set>
+#include <utility>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::ifstream file(argv[1]);
+    std::string line;
+    
+    int rows = 0;
+    int cols = 0;
+    std::set<std::pair<int, int>> coords;
+    // std::vector<std::pair<int, int>> coords;  // (row, col)
+    
+    while (std::getline(file, line)) {
+        if (rows == 0) cols = line.size();
+        
+        for (int c = 0; c < line.size(); c++) {
+            if (line[c] == '@') {  // whatever character you're looking for
+                coords.insert({rows, c});
+                // coords.push_back({rows, c});
+            }
+        }
+        rows++;
+    }
+
+    // for (const auto& [r, c] : coords) {
+    //     std::cout << "(" << r << ", " << c << ")" << std::endl;
+    // }
+
+    int ans = 0;
+    int tmp = 0;
+
+    // for (int r = 0; r < rows; r++) {
+    //     for (int c = 0; c < cols; c++) {
+    for (const auto& [r, c] : coords) {
+            // std::cout << "(" << r << ", " << c << ")" << std::endl;
+            tmp = 0;
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++) {
+                    if (dr == 0 && dc == 0) continue;  // skip self
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    // use (nr, nc)
+                    if (coords.count({nr, nc})) {
+                        // exists
+                        tmp++;
+                    }
+                }
+            }
+            if (tmp < 4) {
+                ans++;
+            }
+    }
+
+    std::cout << "ans is: " << ans << std::endl;
+    return 0;
+
+}
